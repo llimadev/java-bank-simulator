e4bbd7c60f545be90972dd4fd449d7b90e8ee9ee
llimadev <leonel_mk@yahoo.com.br>
2025-08-24 22:44:38 -0300

    Remove IDE files and update gitignore

--- src/com/leonel/javabank/app/BankApp.java
+++ src/com/leonel/javabank/app/BankApp.java
@@ -2,6 +2,7 @@ package com.leonel.javabank.app;
 
 public class BankApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!\n" +
+                "Just removed some files and folders using gitignore!");
     }
 }

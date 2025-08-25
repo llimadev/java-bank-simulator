3c77190f9ed3c7f01b6d87b58789ee3bc1462d13
llimadev <leonel_mk@yahoo.com.br>
2025-08-25 17:38:43 -0300

    feat(user): add getters and setters with basic test

--- src/com/leonel/javabank/app/BankApp.java
+++ src/com/leonel/javabank/app/BankApp.java
@@ -1,8 +1,24 @@
 package com.leonel.javabank.app;
 
+import com.leonel.javabank.model.User;
+import java.util.Scanner;
+
 public class BankApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!\n" +
-                "Just removed some files and folders using gitignore!");
+
+        // Create scanner to read user input
+        Scanner input = new Scanner(System.in);
+
+        User user = new User();
+        // Set default username for demonstration
+        user.setName("Leonel Lima");
+
+        // Prompt user for CPF number and store input
+        System.out.print("Type your CPF number: ");
+        user.setCpf(input.nextLine());
+
+        // Display user information
+        System.out.println("Hello, " + user.getName());
+        System.out.println("Your CPF number is: " + user.getCpf());
     }
 }
--- src/com/leonel/javabank/model/User.java
+++ src/com/leonel/javabank/model/User.java
@@ -1,4 +1,40 @@
 package com.leonel.javabank.model;
 
 public class User {
+    private String name;
+    private String cpf;
+    private String email;
+    private String senha;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String newCpf) {
+        this.cpf = newCpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail (String newEmail) {
+        this.email = newEmail;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String newSenha) {
+        this.senha = newSenha;
+    }
 }

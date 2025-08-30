a28790391f395b7fb40fa8221490861737ebd708
llimadev <leonel_mk@yahoo.com.br>
2025-08-29 23:01:49 -0300

    chore: cleaned-up file to proceed with new tests.

--- src/com/leonel/javabank/app/BankApp.java
+++ src/com/leonel/javabank/app/BankApp.java
@@ -1,20 +1,11 @@
 package com.leonel.javabank.app;
 
-import com.leonel.javabank.exception.AccountValidationException;
 import com.leonel.javabank.model.Account;
 import com.leonel.javabank.model.CheckingAccount;
-import com.leonel.javabank.util.ErrorLogger;
+import com.leonel.javabank.model.User;
 
 public class BankApp {
     public static void main(String[] args) {
 
-        try {
-            Account conta = new CheckingAccount("223456-0");
-            System.out.println(conta);
-        } catch (AccountValidationException e) {
-            System.err.println("Ops! It looks like we ran into a problem " +
-                    "while trying to create your account!" + e.getMessage());
-            ErrorLogger.log(e);
-        }
     }
 }
